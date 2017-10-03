a39c8056158bc339239cfe8573a6ed8e4c960a82
B. Ugur Demirkan <meantcoder@gmail.com>
2017-10-03 22:51:37 +0300

    Fix
    Fixed data pointer

--- gateway.php
+++ gateway.php
@@ -1,7 +1,7 @@
 <?php
     $_pass = $_POST['pass'];
     if(isset($_pass)) {
-        echo "[GATEWAY] => Working, Data collected : " $_pass;
+        echo "[GATEWAY] => Working, Data collected : " . $_pass;
     } else {
         echo "[GATEWAY] => Not working try again :(";
     }
