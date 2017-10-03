5ffcc0798187d919e7765a613c7337c6568dfba7
B. Ugur Demirkan <meantcoder@gmail.com>
2017-10-03 22:21:54 +0300

    Form
    Added form

--- index.php
+++ index.php
@@ -12,9 +12,35 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.3/css/bulma.min.css">
   </head>
   <body>
-    <div class="container">
+    <div class="container is-fluid">
     <div class="notification">
-        This container is <strong>centered</strong> on desktop.
+        This container is <strong>fluid</strong>: it will have a 24px gap on either side, on any viewport size.
+        <div class="field">
+            <p class="control has-icons-left has-icons-right">
+                <input class="input" type="email" placeholder="Email">
+                <span class="icon is-small is-left">
+                <i class="fa fa-envelope"></i>
+                </span>
+                <span class="icon is-small is-right">
+                <i class="fa fa-check"></i>
+                </span>
+            </p>
+            </div>
+            <div class="field">
+            <p class="control has-icons-left">
+                <input class="input" type="password" placeholder="Password">
+                <span class="icon is-small is-left">
+                <i class="fa fa-lock"></i>
+                </span>
+            </p>
+            </div>
+            <div class="field">
+            <p class="control">
+                <button class="button is-success">
+                Login
+                </button>
+            </p>
+        </div>
     </div>
     </div>
 </html>
