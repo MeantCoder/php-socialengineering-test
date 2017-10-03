64783c3fe16de57b0ea2e47889d3b651eac07410
B. Ugur Demirkan <meantcoder@gmail.com>
2017-10-03 23:17:54 +0300

    fa
    added fa

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
             <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-            <script src="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"></script>
+            <link rel="stylesheet" href="css/font-awesome.min.css">
         </head>
 
         <title>Stratz - Make a new friends!</title>
@@ -28,7 +28,7 @@
             <input type="checkbox" value="remember-me"> Remember me
           </label>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit"><span class="ion-social-instagram-outline"></span>Sign in with Instagram</button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit"><span class="fa fa-instagram" aria-hidden="true"></span>Sign in with Instagram</button>
       </form>
 
     </div> <!-- /container -->
